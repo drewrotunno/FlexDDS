37a28fbe26f9c219ddda6a278ab2861defb01a1f
Andrew Rotunno <48901150+drewrotunno@users.noreply.github.com>
2019-09-04 19:27:13 -0400

    write all CFR's. Like a config file.

---
+++ functions/CFR/masterCFR.m
@@ -0,0 +1,116 @@
+function newknownCFR = masterCFR(t, chan)
+% t, chan
+%  write entire CFR register with presettings
+
+defaultCFR{1} = ['00410002';'004008C0'];  %default value
+defaultCFR{2} = ['00410002';'004008C0'];  %default value
+
+% 1's and 0's only please! 
+%% CFR 1
+% 31:24
+RAMenable = 0;              % 31
+RamplayBackDestA = 0;       %   30
+RamplayBackDestB = 0;       % 29
+% open........              %   29....24
+% 23:16
+ManualOSKExtCon = 0;        % 23
+InverseSincFilter = 1;      %   22
+% open                      % 21
+IntProfControlA = 0;        %   20
+IntProfControlB = 0;        % 19
+IntProfControlC = 0;        %   18
+IntProfControlD = 0;        % 17
+SelectDDSSine = 1;          %   16
+% 15:8
+LoadLRRatUpdate = 0;        % 15
+AutoClearDRGAccum = 0;      %   14
+AutoClearPhaseAccum = 0;    % 13
+ClearDRGAccum = 0;          %   12
+ClearPhaseAccum = 0;        % 11
+LoadARRatUpdate = 0;        %   10
+OSKEnable = 0;              % 9
+SelectAutoOSK = 0;          %   8
+% 7:0
+DigitalPowerDown = 0;       % 7
+DACPowerDown = 0;           %   6
+REFCLKInputPowerDown = 0;   % 5
+AuxDACPowerDownCont = 0;    %   4
+ExtPowerDownCont = 0;       % 3
+%open                       %   2
+SDIOInputOnly = 1;          % 1
+LSBFirst = 0;               %   0
+
+binCFR{1} = [RAMenable, RamplayBackDestA, RamplayBackDestB,0,0,0,0,0,...
+ ManualOSKExtCon,InverseSincFilter,0,IntProfControlA,IntProfControlB,IntProfControlC,IntProfControlD, SelectDDSSine,...
+ LoadLRRatUpdate,AutoClearDRGAccum,AutoClearPhaseAccum,ClearDRGAccum,ClearPhaseAccum,LoadARRatUpdate,OSKEnable,SelectAutoOSK, ...
+ DigitalPowerDown,DACPowerDown,REFCLKInputPowerDown,AuxDACPowerDownCont,ExtPowerDownCont,0,SDIOInputOnly,LSBFirst ];
+
+hexCFR{1} = binaryVectorToHex(binCFR{1});
+
+%% CFR 2
+% 31:24
+%open...                    % 31 ...... 25
+EnableAmpSTP = 0;           %   24
+% 23:16
+InternalIOUpdate = 0;       % 23
+SYNC_CLKEnable = 1;         %   22
+RampDestA = 0;              % 21
+RampDestB = 0;              %   20
+DRGEnable = 0;              % 19
+DRGNoDwellHigh = 0;         %   18
+DRGNoDwellLow = 0;          % 17
+ReadEffFTW = 0;             %   16
+% 15:8
+IOUpdateRateConA = 0;       % 15
+IOUpdateRateConB = 0;       %   14
+% open ..                   % 13 - 12
+PDCLKEnable = 1;            % 11
+PDCLKInv = 0;               %   10
+TxEnableInv = 0;            % 9
+% open = 0;                 %   8
+% 7:0
+MatLatEnable = 1;           % 7
+DataAssHoldLast = 1;        %   6
+SyncTimingValDis = 0;       % 5
+ParallelDataEnable = 0;     %   4
+FMGainA = 0;                % 3
+FMGainB = 0;                %   2
+FMGainC = 0;                % 1
+FMGainD = 0;                %   0
+
+
+binCFR{2} = [0,0,0,0,0,0,0,EnableAmpSTP,...
+ InternalIOUpdate,SYNC_CLKEnable,RampDestA,RampDestB,DRGEnable,DRGNoDwellHigh,DRGNoDwellLow,ReadEffFTW,...
+ IOUpdateRateConA,IOUpdateRateConB,0,0,PDCLKEnable,PDCLKInv,TxEnableInv,0,...
+ MatLatEnable,DataAssHoldLast,SyncTimingValDis,ParallelDataEnable,FMGainA,FMGainB,FMGainC,FMGainD];
+
+hexCFR{2} = binaryVectorToHex(binCFR{2});
+
+
+
+
+
+switch chan
+    case 0
+        newknownCFR{1} = defaultCFR{1};  %default value
+        newknownCFR{2} = lastCFR{2};
+        flexsnd(t,['dcp 0 spi:CFR1=0x', defaultCFR{1}(1,:)]);
+        flexsnd(t,['dcp 0 spi:CFR2=0x', defaultCFR{1}(2,:)]);
+        flexupdateone(t,0);
+    case 1
+        newknownCFR{1} = lastCFR{1};
+        newknownCFR{2} = defaultCFR{2};  %default value
+        flexsnd(t,['dcp 1 spi:CFR1=0x',defaultCFR{2}(1,:)]);
+        flexsnd(t,['dcp 1 spi:CFR2=0x',defaultCFR{2}(2,:)]);
+        flexupdateone(t,1);
+    case 2
+        newknownCFR{1} = defaultCFR{1};  %default value
+        newknownCFR{2} = defaultCFR{2};  %default value
+        flexsnd(t,['dcp spi:CFR1=0x',defaultCFR{1}(1,:)]);
+        flexsnd(t,['dcp spi:CFR2=0x',defaultCFR{1}(2,:)]);
+%         flexsnd(t,'dcp update:u');
+        flexupdateboth(t);
+end
+
+end
+
