0a43ba72b319b01e33554879f8ec99fa38072df0
Andrew Rotunno <48901150+drewrotunno@users.noreply.github.com>
2019-09-20 20:00:25 -0400

    ways to sync rephase lock - still a few ns off :(

--- functions/FlexandDCP/flexupdateboth.m
+++ functions/FlexandDCP/flexupdateboth.m
@@ -1,6 +1,9 @@
 function stack = flexupdateboth(stack)
 
+% stack = waitForEvent(stack,2,2);
+% stack = waitForEvent(stack,0,48);
+% stack = waitForEvent(stack,1,48);
+% stack = waitforRackA(stack,2);
 stack = flexstack(stack,'dcp update:u');
 
 end
-
