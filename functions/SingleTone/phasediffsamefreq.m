5d0a8de0bbe6250805b4f7f266a78d78c0dffe9c
Andrew Rotunno <48901150+drewrotunno@users.noreply.github.com>
2019-09-25 12:28:40 -0400

    remove update

--- functions/SingleTone/phasediffsamefreq.m
+++ functions/SingleTone/phasediffsamefreq.m
@@ -3,7 +3,7 @@ function stack = phasediffsamefreq(stack, phasediffdeg, freqHz)
 stack = flexstack(stack,['dcp 0 spi:STP0=','0x',amp2ASF(1),phase2powdeg(0),freq2ftw(freqHz)]);
 stack = flexstack(stack,['dcp 1 spi:STP0=','0x',amp2ASF(1),phase2powdeg(phasediffdeg),freq2ftw(freqHz)]);
 stack = setprof(stack,0);
-stack = flexupdateboth(stack);
+% stack = flexupdateboth(stack);
 
 end
 
