fba41b626e1dbfd6d2df02fe5125c3abe2d610bc
Andrew Rotunno <48901150+drewrotunno@users.noreply.github.com>
2021-07-05 11:14:57 -0400

    remove binaryvector2hex

--- Examples/RampExample.m
+++ Examples/RampExample.m
@@ -3,7 +3,7 @@
 %   if you trigger during the sweep
 % Set initial phase as STP profiles if desired. 
 % Establish a connection to the FlexDDS via IP + slot#
-[t, stack] =openconn('192.168.0.45', 0);
+[t, stack] =openconn('192.168.0.45', 2);
 % t is the TCPIP element. 
 % stack is a running list. We keep adding lines to the -stack-
 % then we -flush- the whole stack at once. 
@@ -18,9 +18,9 @@
 stack = rampdown(stack, 2);
 
 % Variables
-startFreq   =  20e6;        % in Hz
-endFreq     = 120e6;
-time        =   3  ;        % in seconds
+startFreq   =  2e6;        % in Hz
+endFreq     = 200e6;
+time        =   10  ;        % in seconds
 
 % Convert to Words, and Send the Ramp parameters
 [stack, FW, TW, timediff] = rampfreqtime(stack, 2, startFreq, endFreq, time);
@@ -30,18 +30,18 @@ time        =   3  ;        % in seconds
 
 stack = flexupdateboth(stack);
 
-repeat_up_down = 3;
+% repeat_up_down = 1;
 
-for num = 1:1:repeat_up_down
+% for num = 1:1:repeat_up_down
     % now on triggers, change ramp direction
     stack = waitforRackA(stack, 2);
 %     stack = waitForEvent(stack,2,35);     % wait for ramp over 
     stack = rampup(stack, 2);
     
-    stack = waitforRackA(stack, 2);
+%     stack = waitforRackA(stack, 2);
 %     stack = waitForEvent(stack,2,35);     % wait for ramp over 
-    stack = rampdown(stack, 2);
-end
+%     stack = rampdown(stack, 2);
+% end
 % stack'
 
 % Are you guys silly? I'm still gonna send it 
